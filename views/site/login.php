3d5e70cb96eed2a5c77ae1564392bd5cc9b5db89
Alexandr Nikulin <alex.v.nikulin@gmail.com>
2021-08-19 15:42:01 +0300

    add registration link

--- views/site/login.php
+++ views/site/login.php
@@ -35,13 +35,11 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="form-group">
             <div class="offset-lg-1 col-lg-11">
                 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <a class="btn btn-outline-secondary" href="?r=site/registration">Регистрация</a>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
 
-    <div class="offset-lg-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
+
 </div>
